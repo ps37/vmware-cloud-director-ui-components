9f4f280a522c54d87e662fda5fd0c134b30827a0
ps37 <prudhvi.af121@gmail.com>
2020-03-12 19:43:47 -0400

    Added custom action to check components tag to publish latest - v2
    Signed-off-by: ps37 <prudhvi.af121@gmail.com>

--- .github/actions/check-components-tag/index.js
+++ .github/actions/check-components-tag/index.js
@@ -2,4 +2,4 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const tagName = github.context.ref;
-core.setOutput('isComponentsTag', /^i18n-v[0-999].[0-999].[0-999]$/.test(tagName).toString());
+core.setOutput('isComponentsTag', /^components-v[0-999].[0-999].[0-999]$/.test(tagName).toString());
